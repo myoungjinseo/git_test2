82e56ca5745bab89902e7b0a68d1c011687b9ed4
Seo, Myoungjin <sih5673@gmail.com>
2022-09-19 15:45:12 +0900

    Hello 클래스 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -3,6 +3,13 @@ package git_test2;
 public class Hello {
 
 	String message = "안녕하세요";
+	public Hello() {
+		this("안녕하세요");
+	}
+
+	public Hello(String message) {
+		this.message = message;
+	}
 	public void say() {
 		System.out.println(message);
 	}
